b72ff6c5872d197720fd06b4276efd0fdd5f3890
Johan Rylander <johan@rylander.cc>
2014-11-11 22:57:52 +0100

    xx

--- src/main/scala/EulerProblems.scala
+++ src/main/scala/EulerProblems.scala
@@ -2,4 +2,14 @@ object EulerProblems {
   def problem1(): Int = {
     (1 to 999).filter(i => i % 3 == 0 || i % 5 == 0).sum
   }
+
+
+  def fib(n: Int): Int = {
+    if (n == 0) 0
+    else if (n == 1) 1
+    else fib(n-1) + fib(n-2)
+  }
+  def problem2(): Int = {
+    0
+  }
 }
--- src/test/scala/EulerProblemsSpec.scala
+++ src/test/scala/EulerProblemsSpec.scala
@@ -6,4 +6,8 @@ class EulerProblemsSpec extends FlatSpec with Matchers {
   it should "Find the sum of all the multiples of 3 or 5 below 1000" in {
     EulerProblems.problem1() should be (233168)
   }
+
+  it should "By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms" in {
+    EulerProblems.problem2() should be (233168)
+  }
 }
